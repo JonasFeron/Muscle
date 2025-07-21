34ac2f73f839dfa68be384527d9a9e8074abb77f
JonasFeron <jonas.feron@gmail.com>
2025-07-21 14:57:18 +0200

    add DynamicModalAnalysis to Muscle

---
+++ src/Muscle/View/GH_pointMass.cs
@@ -0,0 +1,121 @@
+﻿using Grasshopper.Kernel.Types;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System.Drawing;
+using GH_IO.Serialization;
+using System;
+using MuscleApp.ViewModel;
+
+
+namespace Muscle.View
+{   
+    //GH_PointMass is not a PreviewData for consistency reasons with Element mass. 
+    //To preview the masses, use the output of the Dynamic Solver Component. 
+    public class GH_PointMass : GH_Goo<PointMass> //: GH_GeometricGoo<PointMass>, IGH_PreviewData 
+    {
+        public GH_PointMass() : base()
+        {
+            Value = new PointMass();
+        }
+
+        public GH_PointMass(PointMass mass) : base(mass)
+        {
+            Value = mass;
+        }
+
+        public GH_PointMass(GH_PointMass other)
+        {
+            Value = other.Value.Duplicate();
+        }
+        public override bool IsValid { get { return Value.IsValid; } }
+
+        // public override BoundingBox Boundingbox
+        // {
+        //     get
+        //     {
+        //         BoundingBox bBox = new Line(Value.Point, -10.0 * Value.Vector / Value.Vector.Length, 10.0).BoundingBox;
+        //         bBox.Inflate(1.0);
+        //         return bBox;
+
+
+        //     }
+        // }
+
+        public override string TypeName { get { return "Point mass"; } }
+
+        public override string TypeDescription { get { return "Point mass to apply on the node of a structure."; } }
+
+        // private Color red = Color.Red;
+
+        // public BoundingBox ClippingBox { get { return Boundingbox; } }
+
+        // public void DrawViewportMeshes(GH_PreviewMeshArgs args)
+        // {
+        //     //Take the value of 'DisplayMassAmpli' in the MuscleConfig file to adapt the size of the displayed masses (sphere)
+        //     double DisplayMassAmpli = MuscleConfig.DisplayMassAmpli;
+
+        //     double mass_size = Math.Abs(Value.Vector.Length / 10 * DisplayMassAmpli); //scale x [m] = x[kg]/10kg * MassAmpliFactor
+        //     Point3d point = Value.Point;
+        //     if (mass_size >= 0.001)
+        //     {
+        //         //Sphere to display at each node
+        //         Sphere sph = new Sphere(point, mass_size);
+        //         args.Pipeline.DrawSphere(sph, red);
+        //     }
+        // }
+        
+        // public void DrawViewportWires(GH_PreviewWireArgs args)
+        // {
+
+        //     double DisplayMassAmpli = MuscleConfig.DisplayMassAmpli;
+        //     int _decimal = MuscleConfig.DisplayDecimals;
+
+
+        //     Point3d point = Value.Point;
+
+        //     Plane plane;
+        //     args.Viewport.GetCameraFrame(out plane);
+
+        //     double pixelsPerUnit;
+        //     args.Viewport.GetWorldToScreenScale(point, out pixelsPerUnit);
+        // }
+
+        // public override IGH_GeometricGoo DuplicateGeometry()
+        // {
+        //     return new GH_PointMass(this);
+        // }
+        
+        public override IGH_Goo Duplicate()
+        {
+            return new GH_PointMass(this);
+        }
+
+        // public override BoundingBox GetBoundingBox(Transform xform)
+        // {
+        //     return xform.TransformBoundingBox(Boundingbox);
+        // }
+
+        // public override IGH_GeometricGoo Morph(SpaceMorph xmorph)
+        // {
+        //     GH_PointMass nGHPointMass = new GH_PointMass(this);
+        //     nGHPointMass.Value.Point = xmorph.MorphPoint(Value.Point);
+        //     nGHPointMass.Value.Vector = new Vector3d(xmorph.MorphPoint(new Point3d(Value.Vector)));
+
+        //     return nGHPointMass;
+        // }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        // public override IGH_GeometricGoo Transform(Transform xform)
+        // {
+        //     GH_PointMass nGHPointMass = new GH_PointMass(this);
+        //     nGHPointMass.Value.Point.Transform(xform);
+        //     nGHPointMass.Value.Vector.Transform(xform);
+
+        //     return nGHPointMass;
+        // }
+    }
+}
