c4479ba566609191d7426dc1ae994b99cf24904a
JonasFeron <jonas.feron@gmail.com>
2025-07-21 14:47:27 +0200

    add DynamicModalAnalysis to MuscleCore

--- src/MuscleCore/Converters/Main.cs
+++ src/MuscleCore/Converters/Main.cs
@@ -55,7 +55,7 @@ namespace MuscleCore.Converters
             PyObjectConversions.RegisterDecoder(new PyNodesDecoder());
             PyObjectConversions.RegisterDecoder(new PyElementsDecoder());
             PyObjectConversions.RegisterDecoder(new PyResultsSVDDecoder());
-
+            PyObjectConversions.RegisterDecoder(new PyResultsDynamicDecoder());
 
         }
     }
